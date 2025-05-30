999ab9242edd4bbc1202ed60ea250cfa4688d925
freya <freya.aino@pm.me>
2025-05-31 00:08:41 +0200

    device-node, main.rs updated, not complete

--- device-node/src/main.rs
+++ device-node/src/main.rs
@@ -1,22 +1,80 @@
-use devices::{get_all_camera_devices_linux, get_all_microphone_devices};
+use clap::{Parser, Subcommand};
+use devices::{CpalMicrophoneDevice, DeviceManager, V4lCameraDevice};
+use shared::start_static_http_server;
+use std::{env::consts::OS, process::Command};
+
+#[derive(Parser)]
+#[command(name = "device-node")]
+struct Cli {
+    #[command(subcommand)]
+    command: ClapCommand,
+}
+
+#[derive(Subcommand)]
+enum ClapCommand {
+    Init,
+    List {
+        #[arg(short, long)]
+        verbose: bool,
+    },
+    Run {
+        #[arg(short, long, default_value_t = String::from("0.0.0.0"))]
+        host: String,
+        #[arg(short, long, default_value_t = 8080)]
+        port: u16,
+    },
+}
 
 fn main() {
-    let cpal_host = cpal::default_host();
+    assert_eq!(OS, "linux", "Unsupported OS: {}", OS);
 
     // get all devices
-    let camera_devices = get_all_camera_devices_linux().unwrap();
-    let microphone_devices = get_all_microphone_devices(&cpal_host).unwrap();
+    let cpal_host = cpal::default_host();
+    let camera_dms = DeviceManager::<V4lCameraDevice>::get_all_devices().unwrap();
+    let microphone_dms =
+        DeviceManager::<CpalMicrophoneDevice>::get_all_devices(&cpal_host).unwrap();
 
-    println!("--> {} camera devices found:", camera_devices.len());
-    println!("--> {} microphone devices found:", microphone_devices.len());
+    // cli commands
+    match Cli::parse().command {
+        ClapCommand::Init => {
+            println!("Initializing device node...");
+            println!("Creating cache directory...");
+            let mut home_path = std::env::var("HOME").unwrap();
+            home_path.push_str("/.cache/device-capture-device-node/");
+            let out = Command::new("mkdir")
+                .arg("-p")
+                .arg(home_path)
+                .output()
+                .unwrap();
+            println!(
+                "{}{}",
+                String::from_utf8_lossy(&out.stderr),
+                String::from_utf8_lossy(&out.stdout)
+            );
 
-    for device in camera_devices {
-        println!("{:?}", device.device_info);
+            println!("Saving Configuration")
+        }
+        ClapCommand::List { verbose } => {
+            println!("--> {} camera devices found:", camera_dms.len());
+
+            for dm in camera_dms {
+                println!("{:?}", dm.device.device_info);
+
+                if verbose {}
+            }
+
+            println!("--> {} microphone devices found:", microphone_dms.len());
+
+            for dm in microphone_dms {
+                println!("{:?}", dm.device.device_info);
+
+                if verbose {}
+            }
+        }
+        ClapCommand::Run { host, port } => {
+            println!("Running device node on {}:{}...", host, port);
+        }
     }
-    for device in microphone_devices {
-        println!("{:?}", device.device_info);
-    }
-    // get all configurations for all devices
-    // open a http server to advertise the device control
-    //
+
+    // start_static_http_server("0.0.0.0", "8080", ())
 }
