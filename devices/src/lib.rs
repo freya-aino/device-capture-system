ea8c3c39a17b55872aa89049bfd3104657e3e1ea
freya <freya.aino@pm.me>
2026-04-04 11:33:13 +0200

    build check, bugfix

--- devices/src/lib.rs
+++ devices/src/lib.rs
@@ -4,11 +4,13 @@ mod manager;
 pub use camera::*;
 
 #[cfg(target_os = "linux")]
-mod microphone_linux {
-    pub use microphone_linux::*;
-}
+mod microphone_linux;
+
+#[cfg(target_os = "linux")]
+pub use microphone_linux::*;
 
 #[cfg(target_os = "windows")]
-mod microphone_windows {
-    pub use microphone_windows::*;
-}
+mod microphone_windows;
+
+#[cfg(target_os = "windows")]
+pub use microphone_windows::*;
